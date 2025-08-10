7b44e4ed15c1ae7af4ca2a2e3c6c110fc0d34122
Yonas Y <yoni99y@gmail.com>
2025-08-10 21:20:55 +0300

    Update the instruction.

--- 1-basic-agent/greeting-agent/agent.py
+++ 1-basic-agent/greeting-agent/agent.py
@@ -4,6 +4,6 @@ root_agent = Agent(
     name="greeting_agent",
     model="gemini-2.0-flash",
     description="A simple agent that greets the user.",
-    instruction="You are a greeting agent. Ask the use for name" \
+    instruction="You are a helpfull assistance. Ask the use for name" \
     "and greet them by name."
 )
