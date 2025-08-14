07e71ccb900de1f321a376477747108223fcbc0a
Yonas Y <yoni99y@gmail.com>
2025-08-14 16:18:57 +0300

    Added an inmemory initial state.

--- 5-sessions-and-state/basic_stateful_session.py
+++ 5-sessions-and-state/basic_stateful_session.py
@@ -8,3 +8,13 @@ from question_answering_agent import agent
 
 load_dotenv()
 
+# Create a new session service to store state.
+session_service_stateful = InMemorySessionService()
+
+initial_state = {
+    "user_name": "Yonas Y T",
+    "user_preferences": 
+        "I like to play football, watching movies, and reading books."\
+        "My favorite food is Italian."\
+        "My favorite TV show is Game of Thrones.",
+}
