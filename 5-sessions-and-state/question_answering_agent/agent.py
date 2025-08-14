65e2304eae9599637d90bec043e7c6d608e3d149
Yonas Y <yoni99y@gmail.com>
2025-08-14 16:03:46 +0300

    Created a simple question answering agent.

---
+++ 5-sessions-and-state/question_answering_agent/agent.py
@@ -0,0 +1,13 @@
+from google.adk.agents import Agent
+
+root_agent = Agent(
+    name = "question_answering_agent",
+    model = "gemini-2.0-flash",
+    description= "Question answering agent",
+    instruction= "You are a helpful assistant that answers questions about the user's preferences." \
+    "" \
+    "Here is some information about the user:" \
+    "Name: {user_name}" \
+    "Preferences: {user_preferences}",
+)
+
