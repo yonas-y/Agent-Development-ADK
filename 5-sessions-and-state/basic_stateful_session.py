9bef52d688bf9914710f0708dc568ac3d64a18e7
Yonas Y <yoni99y@gmail.com>
2025-08-14 16:17:59 +0300

    Added the required packages.

---
+++ 5-sessions-and-state/basic_stateful_session.py
@@ -0,0 +1,10 @@
+import uuid
+
+from dotenv import load_dotenv
+from google.adk.runners import Runner
+from google.adk.sessions import InMemorySessionService
+from google.genai import types
+from question_answering_agent import agent
+
+load_dotenv()
+
