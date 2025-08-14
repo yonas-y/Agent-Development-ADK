1db07c892a6aa23861d7a1890e3989d042a98a31
Yonas Y <yoni99y@gmail.com>
2025-08-14 16:48:15 +0300

    Updated to examine the session properties.

--- 5-sessions-and-state/basic_stateful_session.py
+++ 5-sessions-and-state/basic_stateful_session.py
@@ -1,8 +1,8 @@
 import uuid
-
+import asyncio
 from dotenv import load_dotenv
 from google.adk.runners import Runner
-from google.adk.sessions import InMemorySessionService
+from google.adk.sessions import InMemorySessionService, Session 
 from google.genai import types
 from question_answering_agent import agent
 
@@ -18,3 +18,27 @@ initial_state = {
         "My favorite food is Italian."\
         "My favorite TV show is Game of Thrones.",
 }
+
+# Create a NEW session.
+APP_NAME = "YON Bot"
+USER_ID = "yoni_99"
+SESSION_ID = str(uuid.uuid4())
+
+async def main():
+    stateful_session = await session_service_stateful.create_session(
+        app_name=APP_NAME,
+        user_id=USER_ID,
+        session_id=SESSION_ID,
+        state=initial_state,
+    )
+
+    print(f"--- Examining Session Properties ---")
+    print(f"\tID {'id'}: {stateful_session.id}")
+    print(f"\tApplication Name (`app_name`): {stateful_session.app_name}")
+    print(f"\tUser ID (`user_id`): {stateful_session.user_id}")
+    # print(f"\tState (`state`): {stateful_session.state}") # Note: Only shows initial state here
+    print(f"\tEvents (`events`): {stateful_session.events}") # Initially empty
+    print(f"\tLast Update (`last_update_time`): {stateful_session.last_update_time:.2f}")
+    print(f"---------------------------------")
+
+asyncio.run(main())
