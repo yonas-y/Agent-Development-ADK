0a3930fe2d9d432760c0333c51ce977f60d78e2b
Yonas Y <yoni99y@gmail.com>
2025-08-14 17:03:31 +0300

    Changed the agent name.

--- 5-sessions-and-state/question_answering_agent/agent.py
+++ 5-sessions-and-state/question_answering_agent/agent.py
@@ -1,6 +1,6 @@
 from google.adk.agents import Agent
 
-root_agent = Agent(
+question_answering_agent = Agent(
     name = "question_answering_agent",
     model = "gemini-2.0-flash",
     description= "Question answering agent",
@@ -11,3 +11,33 @@ root_agent = Agent(
     "Preferences: {user_preferences}",
 )
 
+
+
+
+
+
+# new_message = types.Content(
+#     role="user",
+#     parts=[types.Part(text="What is my favorite food?")],
+# )
+
+# for event in runner.run(
+#     user_id=USER_ID,
+#     session_id=SESSION_ID,
+#     new_message=new_message,
+# ):
+#     if event.is_final_response():
+#         if event.content and event.content.parts:
+#             print(f"Final response: {event.content.parts[0].text}")
+
+
+# print("Session event exploration....:")
+# session = session_service_stateful.get_session(
+#     app_name=APP_NAME,
+#     user_id=USER_ID,
+#     session_id=SESSION_ID,
+# )
+
+# print("Final session state:")
+# for key, event in session.state.items():
+#     print(f"\t{key}: {event}")
